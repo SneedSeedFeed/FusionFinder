f91945bdef58d337b45527a497517745297d7358
Adam <a.stopher11@newcastle.ac.uk>
2023-11-14 23:59:49 +0000

    Fixed types, probably

--- fusion_finder-8717d2d044554f21.js
+++ fusion_finder-8717d2d044554f21.js
@@ -541,7 +541,7 @@ function __wbg_get_imports() {
         const ret = makeMutClosure(arg0, arg1, 41, __wbg_adapter_22);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper416 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper415 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 170, __wbg_adapter_30);
         return addHeapObject(ret);
     };
@@ -584,7 +584,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('fusion_finder-9bb62bf33edb5858_bg.wasm', import.meta.url);
+        input = new URL('fusion_finder-8717d2d044554f21_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
